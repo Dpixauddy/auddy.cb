cd9fc057ddb0b07b1f18b683cef084fd0741593a
Dpixauddy <99086561+Dpixauddy@users.noreply.github.com>
2022-02-22 21:39:34 +0700

    Add files via upload

---
+++ start.py
@@ -0,0 +1,24 @@
+baris (23 sloc)  754 Byte
+impor  os , sys , waktu , permintaan
+os . sistem ( "jelas" )
+print ( "Kamu Belum Masuk Login Kelebihan Dahulu" )
+print ( "Database Agar Data Kamu Masuk Ke" )
+print ( " \033 [1;92m_ ____ ____ _ _ _ " )
+print ( " MR AUDDY " )
+
+x  =  "Tuhan Ammar"
+y  =  "berlangganan"
+def  masuk ():
+          user  =  raw_input ( "Masukkan Nama Pembuat : " )
+          pasw  =  raw_input ( "Masukkan Password : " )
+	  nama  =  raw_input ( "Masukkan Nomor HP Kamu : " )
+          jika  pengguna  == x  dan  pasw  == y :
+              print ( "Akses diberikan" )
+              waktu . tidur ( 2 )
+	      os . sistem ( "python2 menu.py" )
+          lain :
+              print ( "Akses danied" )
+              waktu . tidur ( 2 )
+              os . sistem ( "python2 start.py" )
+jika  __name__  ==  "__main__" :
+       masuk ()
