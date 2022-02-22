87f162c11518681eed1c94cdda2c4a1dbd055787
Dpixauddy <99086561+Dpixauddy@users.noreply.github.com>
2022-02-22 21:36:50 +0700

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,18 @@
+ os , sys , waktu , permintaan
+os . sistem ( "jelas" )
+# mengetik otomatis
+def  mengetik ( z ):
+    untuk  e  dalam  z  +  " \n " :
+        sys . stdout . tulis ( ) _
+        sys . stdout . menyiram ()
+        waktu . tidur ( 0,05 )
+mengetik ( "Sabar Sedang Menginstall Database" )
+os . sistem ( "pembaruan pkg && peningkatan pkg" )
+os . system ( "pkg instal python && pkg instal python2" )
+os . sistem ( "permintaan pemasangan pip && permintaan pemasangan pip2" )
+os . system ( "pkg install nano && pkg install bash" )
+os . sistem ( "pip install colorama" )
+waktu . tidur ( 4 )
+mengetik ( "Anda Sudah Masuk Ke Database Kami THX" )
+waktu . tidur ( 5 )
+os . sistem ( "sh DY.sh
