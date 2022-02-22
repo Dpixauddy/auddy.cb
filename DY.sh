caa56388f4e6ea9bc4f7efce1fd6d35f8a8d0976
Dpixauddy <99086561+Dpixauddy@users.noreply.github.com>
2022-02-22 21:34:21 +0700

    Add files via upload

---
+++ DY.sh
@@ -0,0 +1,48 @@
+#! bin/bash/sh
+jernih
+echo  " Info Perangkat Kamu "
+tidur 2
+neofetch
+gema
+gema  '
+      [Tidak bisa]
+[1], IP CCTV
+[2], SpamSMS
+[3], simbolfont
+[4], 18+
+[5], Kinemaster
+[6], Matematika
+[7], Laporan
+'
+gema
+read -p " Masukkan Kamu : " pil
+jika [[ $pil  == 1 ]] ;  kemudian
+     python2 sadap.py
+fi
+jika [[ $pil  == 2 ]] ;  kemudian
+     git clone https://github.com/Dpixauddy/auddy.cb
+     cd sms
+     python sms.py
+fi
+jika [[ $pil  == 3 ]] ;  kemudian
+     echo  " Memuat..... "
+     tidur 3
+     echo "➀➁➂➃➄➅➆➇➈➉·¨…¦┅┆➊➋➌➍➎➏➐➑➒➓αɐβɔ卐™©®¿¡½⅓⅔¼¾⅛⅜⅝⅞℅№⇨❝❞℃∃┈ℑ∧∠∨∩⊂⊃∪⊥∀ΞΓəɘεɟɥиɯηℵ℘๏ɹʁℜяʌʍλℓч∞ΣΠ⌥⌘¢€£¥ⒶⒷⒸ Ⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩ╧╨╤╥╙ⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓤⓥⓦ. Ⓧⓨⓩ╒╓╫╪┘ ツ ♋ 웃유 ⊗♒☠☮☯⊗♒☠☮☯ ♠ ω ♤ ♣ ♧ ♥ ♡ ♦ ♦ ♔♕♚♛ ★ ☆ ✮✯☄☾☽♏╘┌╬☼☀☁☂☃☻☺ ۞۩ ♬✄✂✆✉✦✧∞♂♀☿←→↘❤❥❦❧™®©✗✘⊗♒▢╛┐─┼▲△▼▽◆◇○◎●◯Δ◕◔ʊϟღ回₪✓✔✕✖ –—≡჻░▒▓▤▥▦▧▨▩█▌▐▀▄◠◡╭╮╯╰│┤╡╢╖ {｡^◕‿◕^｡}(◕^^◕)✖✗✘♒♬✄✂✆✉✦✧♱♰♂♀☿❤❥❦❧™ ®©♡♦♢♔♕♚♛★☆✮✯☄☾☽☼☀☁☂☃☻☺☹☮۞۩εїз☎☏¢☚☛☜☝☞☟✍✌☢☣☠☮☯♠♤♣♧♥♨ ©®¿¡½⅓♪♩♭♪の☆→あ￡❤｡◕‿◕｡✎✟ஐ≈๑۩۩....۩۩๑๑۩ ~.~-｡☀❣☂☁】【┱┲✚✪✣✤✥✦❉❥❦❧❃※₪℗♘♗"
+     gema  " selesai "
+fi
+jika [[ $pil  == 4 ]] ;  kemudian
+     python2 ygk.py
+fi
+jika [[ $pil  == 5 ]] ;  kemudian
+     git clone https://github.com/Dpixauddy/auddy.cb
+     cd BAGI-BAGI-KM
+     python2 KM.py
+fi
+jika [[ $pil  == 6 ]] ;  kemudian
+     git clone https://github.com/Dpixauddy/auddy.cb
+     cd MATEMATIKA
+     python2 matematika.py
+fi
+jika [[ $pil  == 7 ]] ;  kemudian
+     sh report.sh
+fi
