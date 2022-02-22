b24da66208b0a3f3783e217974133cac6d6acecb
Dpixauddy <99086561+Dpixauddy@users.noreply.github.com>
2022-02-22 21:37:49 +0700

    Add files via upload

---
+++ report.sh
@@ -0,0 +1,17 @@
+#! bash/bin/sh
+biru= " \033[34;1m "
+hijau= " \033[32;1m "
+ungu= " \033[35;1m "
+cyan= " \033[36;1m "
+merah= " \033[31;1m "
+echo  " Contoh Report = jika Muncul Pertanyaan Langsung "
+echo  " Kirim Bug/Ke eror an Dalam Script [!] Catatan: "
+echo  " Laporan Main-Main Tidak Akan Ditanggapi!!! "
+tidur 5
+echo  " Halo Teman Ada Yang Bisa Saya Bantu?? "
+read -p " Ada Yang Bisa Saya Bantu : " bntu
+tidur 2
+echo  $green " $bntu ,Laporkan Berhasil Dikirim Ke Penulis!!! "
+echo  " Terima Kasih Telah Melaporkan Kendala Script Error "
+echo  " Berikut Nomor Whatsapp Penulis "
+gema  " https://wa.me/625719898124 "
