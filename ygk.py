ce878ee52b1d858be561de32a425ad14accb3a1f
Dpixauddy <99086561+Dpixauddy@users.noreply.github.com>
2022-02-22 21:40:01 +0700

    Add files via upload

---
+++ ygk.py
@@ -0,0 +1,28 @@
+impor  os , sys , waktu , permintaan
+os . sistem ( "jelas" )
+# mengetik otomatis
+def  mengetik ( z ):
+    untuk  e  dalam  z  +  " \n " :
+        sys . stdout . tulis ( ) _
+        sys . stdout . menyiram ()
+        waktu . tidur ( 0,001 )
+
+print ( "Perangkat Kamu" )
+os . sistem ( "neofetch" )
+print ( """ \033 [1;0m \033 [1;41m( B ) ( ) ( k ) ( e ) ( p ) \033 [1;0m""" )
+
+waktu . tidur ( 5 )
+mengetik ( """ \033 [1;92m .https://www.mediafire.com/file/h2nygxbyb6n9cyo/VID-20210107-WA1468.mp4/file
+.https://www.mediafire.com/file/pk8hozohzdc076c/VID-20210107-WA1466.mp4/file
+.https://www.mediafire.com/file/pk8hozohzdc076c/VID-20210107-WA1466.mp4/file
+.https://www.mediafire.com/file/arpphhxsv94ak0r/VID-20210107-WA1462.mp4/file
+https://www.mediafire.com/file/us3f4j62emftbrf/VID-20210107-WA1463.mp4/file
+https://www.mediafire.com/file/v4033tkl16hgf2b/VID-20210107-WA1459.mp4/file
+https://www.mediafire.com/file/3scnim6d1x4b8ie/VID-20210107-WA1461.mp4/file
+https://www.mediafire.com/file/dx9tklonu0eq36w/VID-20210107-WA1464.mp4/file
+https://www.mediafire.com/file/snwja297dv4zvtl/VID-20210107-WA0036.mp4/file
+https://www.mediafire.com/file/60dqek0mqhyt6rn/VID-20210107-WA1530.mp4/file
+https://www.mediafire.com/file/ni2mcdknb6zn50t/VID-20210107-WA1532.mp4/file
+""" )
+print ( "Hak Cipta Sampai jumpa LordAmmar" )
+cetak ( "Selesai" )
